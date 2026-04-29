77d41ef76c02b742fa3e4cc8f3dbca1176d77020
Skyler Harris <jeanettearkenbergdejafe71@gmail.com>
2026-04-29 09:45:50 +0200

    Refactor game into a class with better structure

--- snake.py
+++ snake.py
@@ -1,68 +1,107 @@
 import curses
 import random
-import time
+
+class SnakeGame:
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
+        self.h, self.w = stdscr.getmaxyx()
+        self.score = 0
+        self.game_speed = 100
+        self.snake = [(self.h // 2, self.w // 4 + i) for i in range(3)]
+        self.direction = curses.KEY_RIGHT
+        self.food = self._place_food()
+        self.game_over = False
+
+    def _place_food(self):
+        while True:
+            pos = (random.randint(1, self.h - 2), random.randint(1, self.w - 2))
+            if pos not in self.snake:
+                return pos
+
+    def handle_input(self):
+        key = self.stdscr.getch()
+        if key == ord("q"):
+            self.game_over = True
+            return
+        opposite = {
+            curses.KEY_UP: curses.KEY_DOWN,
+            curses.KEY_DOWN: curses.KEY_UP,
+            curses.KEY_LEFT: curses.KEY_RIGHT,
+            curses.KEY_RIGHT: curses.KEY_LEFT,
+        }
+        if key in opposite and key != opposite.get(self.direction):
+            self.direction = key
+
+    def update(self):
+        head = self.snake[-1]
+        moves = {
+            curses.KEY_UP: (-1, 0),
+            curses.KEY_DOWN: (1, 0),
+            curses.KEY_LEFT: (0, -1),
+            curses.KEY_RIGHT: (0, 1),
+        }
+        dy, dx = moves[self.direction]
+        new_head = (head[0] + dy, head[1] + dx)
+
+        # Wall collision
+        if (new_head[0] <= 0 or new_head[0] >= self.h - 1 or
+            new_head[1] <= 0 or new_head[1] >= self.w - 1):
+            self.game_over = True
+            return
+
+        # Self collision
+        if new_head in self.snake:
+            self.game_over = True
+            return
+
+        self.snake.append(new_head)
+
+        if new_head == self.food:
+            self.score += 1
+            self.food = self._place_food()
+            # Speed up slightly
+            self.game_speed = max(50, self.game_speed - 2)
+            self.stdscr.timeout(self.game_speed)
+        else:
+            tail = self.snake.pop(0)
+            self.stdscr.addch(tail[0], tail[1], " ")
+
+    def draw(self):
+        if self.game_over:
+            return
+        head = self.snake[-1]
+        self.stdscr.addch(head[0], head[1], "S")
+        self.stdscr.addch(self.food[0], self.food[1], "o")
+        self.stdscr.addstr(0, 2, f" Score: {self.score} | Speed: {100 - self.game_speed + 50} ")
+
+    def show_game_over(self):
+        msg = f"GAME OVER! Score: {self.score}"
+        self.stdscr.addstr(self.h // 2, self.w // 2 - len(msg) // 2, msg)
+        self.stdscr.addstr(self.h // 2 + 1, self.w // 2 - 10, "Press any key to exit")
+        self.stdscr.nodelay(0)
+        self.stdscr.getch()
+
+    def run(self):
+        curses.curs_set(0)
+        self.stdscr.nodelay(1)
+        self.stdscr.timeout(self.game_speed)
+
+        # Draw initial food
+        self.stdscr.addch(self.food[0], self.food[1], "o")
+
+        while not self.game_over:
+            self.handle_input()
+            if not self.game_over:
+                self.update()
+            if not self.game_over:
+                self.draw()
+
+        self.show_game_over()
+
 
 def main(stdscr):
-    curses.curs_set(0)
-    stdscr.nodelay(1)
-    stdscr.timeout(100)
-
-    h, w = stdscr.getmaxyx()
-
-    # Initial snake
-    snake = [(h // 2, w // 4 + i) for i in range(3)]
-    direction = curses.KEY_RIGHT
-
-    # Initial food
-    food = (random.randint(1, h - 2), random.randint(1, w - 2))
-    stdscr.addch(food[0], food[1], "o")
-
-    score = 0
-
-    while True:
-        key = stdscr.getch()
-
-        if key == ord("q"):
-            break
-
-        if key in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT]:
-            opposite = {curses.KEY_UP: curses.KEY_DOWN, curses.KEY_DOWN: curses.KEY_UP,
-                       curses.KEY_LEFT: curses.KEY_RIGHT, curses.KEY_RIGHT: curses.KEY_LEFT}
-            if key != opposite.get(direction):
-                direction = key
-
-        head = snake[-1]
-        if direction == curses.KEY_UP:
-            new_head = (head[0] - 1, head[1])
-        elif direction == curses.KEY_DOWN:
-            new_head = (head[0] + 1, head[1])
-        elif direction == curses.KEY_LEFT:
-            new_head = (head[0], head[1] - 1)
-        elif direction == curses.KEY_RIGHT:
-            new_head = (head[0], head[1] + 1)
-
-        # Check collision
-        if (new_head[0] <= 0 or new_head[0] >= h - 1 or
-            new_head[1] <= 0 or new_head[1] >= w - 1 or
-            new_head in snake):
-            break
-
-        snake.append(new_head)
-
-        if new_head == food:
-            score += 1
-            food = (random.randint(1, h - 2), random.randint(1, w - 2))
-            stdscr.addch(food[0], food[1], "o")
-        else:
-            tail = snake.pop(0)
-            stdscr.addch(tail[0], tail[1], " ")
-
-        stdscr.addch(new_head[0], new_head[1], "S")
-        stdscr.addstr(0, 2, f" Score: {score} ")
-
-    stdscr.addstr(h // 2, w // 2 - 5, f"GAME OVER! Score: {score}")
-    stdscr.nodelay(0)
-    stdscr.getch()
+    game = SnakeGame(stdscr)
+    game.run()
 
 if __name__ == "__main__":
     curses.wrapper(main)
